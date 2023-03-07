28c19c46b50f399b644225b992290bb159ba376e
AhsanKhalid1 <l192731@lhr.nu.edu.pk>
2023-03-07 18:18:22 +0500

    first commit

---
+++ src/utils/data.js
@@ -0,0 +1,169 @@
+import { HiOutlineDesktopComputer } from "react-icons/hi";
+import { CiMobile1 } from "react-icons/ci";
+import { MdWorkspacesOutline } from "react-icons/md";
+export const projectExperience = [
+  {
+    name: "Web",
+    projects: 36,
+    icon: HiOutlineDesktopComputer,
+    bg: "#286F6C",
+  },
+  {
+    name: "Android",
+    projects: 23,
+    icon: CiMobile1,
+    bg: "#EEC048",
+  },
+  {
+    name: "Design",
+    projects: 19,
+    icon: MdWorkspacesOutline,
+    bg: "#F26440",
+  },
+];
+
+export const WhatDoIHelp = [
+  "I will help you with finging a solution and solve your problem. Passionate to craft innovative web and mobile products.",
+];
+
+export const workExp = [
+  {
+    place: "Be My Voice",
+    tenure: "2022*",
+    role: "AI Based Android App",
+    detail:
+      "Artificial Intelligence based real time android application made with the help of Pakistan Sign Language(PSL)",
+  },
+  {
+    place: "TEXTIFY",
+    tenure: "2022",
+    role: "Web App",
+    detail:
+      "A web based application developed using the MERN stack. All the user passwords are stored on MongoDB encrypted.",
+  },
+  {
+    place: "Face Recognition",
+    tenure: "2021-22",
+    role: "Artificial Intelligence",
+    detail:
+      "An Artificial Intelligence based program made in python which helps in face detection using cv2 library.",
+  },
+  {
+    place: "Tic Tac Toe",
+    tenure: "2020-21",
+    role: "Assembly Language",
+    detail:
+      "A 2 player game where a player has to draw a pattern of the same shapes either vertically, horizontally and diagonally in Assembly language.",
+  },
+  {
+    place: "Emotion Detection App",
+    tenure: "2023",
+    role: "Artificial Intelligent Android App",
+    detail:
+      "An Artificial Intelligence based real time android application which helps in detecting emotions of users",
+  }
+];
+
+export const comments = [
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people1.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people1.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people1.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people1.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+];
+
+export const sliderSettings = {
+  dots: true,
+  infinite: false,
+  speed: 1000,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  initialSlide: 0,
+  touchMove: true,
+  useCSS: true,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
